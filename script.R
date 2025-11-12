8d7870c1ac6640710f69f0fd96fe25f5487da130
batistone5 <samuelbatistone@id.uff.br>
2025-11-12 10:24:05 -0300

    Atualizando script.

--- script.R
+++ script.R
@@ -1,11 +1,24 @@
+# Pacotes ----
+
 library(tidyverse)
 library(readxl)
 library(ggplot2)
 
+# Base ----
+
 base = read_excel("Base Casos.xlsx")
 
-#Boxplot estatura
+# Boxplot estatura ----
 
  base %>% 
   ggplot(mapping = aes(y = base$Estatura)) + 
   geom_boxplot(fill = "red")
+
+ 
+ # Boxplot Idade ----
+ 
+ base %>% 
+   ggplot(mapping = aes(y = base$Idade)) + 
+   geom_boxplot(fill = "dodgerblue")
+ 
+ 
